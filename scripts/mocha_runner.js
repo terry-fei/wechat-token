f2f1b6fa098f7edd416f5d663c21fa04d7f90117
feit <i@feit.me>
2016-02-07 16:19:17 +0800

    init

---
+++ scripts/mocha_runner.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+require('babel-core/register');
+require('babel-polyfill');
+
+process.on('unhandledRejection', function (error) {
+  console.error('Unhandled Promise Rejection:');
+  console.error(error && error.stack || error);
+});
